b76f96cebb5c95ed0ee4a1c240cb3eed63876a26
Taddy95 <whitneytaddyev@gmail.com>
2021-12-13 16:41:37 +0000

    Javascript

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,9 @@
 #!/usr/bin/node
 const argValue = process.argv;
-
-if (argValue.length < 3)
-{
-	console.log('No argument');
+if (argValue.length < 3) {
+  console.log('No argument');
 } else if (argValue.length === 3) {
-	console.log('Argument found');
+  console.log('Argument found');
 } else {
-	console.log('Arguments found');
+  console.log('Arguments found');
 }
--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
 const myVal = process.argv;
-
-if (myVal[2] === undefined)
-{
-	console.log('No argument');
+if (myVal[2] === undefined) {
+  console.log('No argument');
 } else {
-	console.log(myVal[2]);
+  console.log(myVal[2]);
 }
