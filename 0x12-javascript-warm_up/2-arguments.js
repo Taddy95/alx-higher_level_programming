df2f6247b6528e2165ac2b97ef3b7db7fe7905a7
Taddy95 <whitneytaddyev@gmail.com>
2021-12-13 16:28:24 +0000

    Javascript

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const argValue = process.argv;
+
+if (argValue.length < 3)
+{
+	console.log('No argument');
+} else if (argValue.length == 3)
+{
+	console.log('Argument found');
+} else
+{
+	console.log('Arguments found');
+}
