d4505d76ddd3bb5b340716812f42ae04f31dc858
Taddy95 <whitneytaddyev@gmail.com>
2021-12-14 12:57:11 +0000

    Javascript

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const myVal = parseInt(process.argv[2]);
+if (myVal) {
+  for(let i = 0; i < myVal; i++) {
+  console.log('C is fun');
+  }
+} else {
+  console.log('Missing number of occurrences');
+}
